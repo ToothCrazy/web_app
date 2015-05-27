4755a801c05e4152fb5b3f9c61f492008afe4b7d
chenchengbest <chenchengbest1@163.com>
2015-05-27 22:51:05 +0800

    新增requirejs功能

--- src/common/require.config.js
+++ src/common/require.config.js
@@ -1,7 +1,7 @@
 require.config({
 		baseUrl: "/src",
 		paths: {
-			JQMobile:"common/jquery.mobile-1.3.2.min.js",
-			jquery:"common/jquery-1.8.3.min.js"
+			JQMobile:"common/jquery.mobile-1.3.2.min",
+			jquery:"common/jquery-1.8.3.min"
 		}
     });
